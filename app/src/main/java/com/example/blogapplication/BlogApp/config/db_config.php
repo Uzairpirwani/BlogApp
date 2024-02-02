e4daa39291fe1b4fa4edde966fb130d3c3a7ffa5
UzairPirwani <muzair@miletap.com>
2024-02-02 17:00:58 +0500

    initial commit

--- app/src/main/java/com/example/blogapplication/BlogApp/config/db_config.php
+++ app/src/main/java/com/example/blogapplication/BlogApp/config/db_config.php
@@ -1,3 +1,20 @@
 <?php
  $connection = mysqli_connect('localhost','root','','db_blog_app')
 ?>
+
+/*
+CREATE TABLE blogUser (
+    userId INT AUTO_INCREMENT PRIMARY KEY,
+    userName VARCHAR(255) NOT NULL,
+    userEmail VARCHAR(255) NOT NULL,
+    userPassword VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Blog (
+    blogId INT AUTO_INCREMENT PRIMARY KEY,
+    blogName VARCHAR(255) NOT NULL,
+    blogDescription TEXT
+);
+
+
+ */
